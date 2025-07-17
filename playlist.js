54245d77fecb3826537341606eb89c99d41e2efd
Pratik Gurung <pratikgurungsjg@gmail.com>
2025-07-17 18:00:42 +0545

    about section updated with experience timeline

--- playlist.js
+++ playlist.js
@@ -4,19 +4,19 @@ export const playlist = [
     {
       title: "फूल [Phool]",
       artist: "Sujan Chapagain",
-      src: "/public/Phool - ऊनको स्वीटर OST.mp3", // Corrected path
-      albumArt: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJyI5bd2BYlLxUV2nBvs5cSGJKlQKQryNoGg&s", // Placeholder image
+      src: "/assets/songs/Phool - ऊनको स्वीटर OST.mp3", // Corrected path
+      albumArt: "https://encrypted-tbn0.gstatic.com/assets/images?q=tbn:ANd9GcSJyI5bd2BYlLxUV2nBvs5cSGJKlQKQryNoGg&s", // Placeholder image
     },
     {
       title: "Kasari Ma Bhule",
       artist: "Gaurav Rai",
-      src: "/public/Kasari Ma Bhuley Cover.mp3", // Corrected path
+      src: "/assets/songs/Kasari Ma Bhuley Cover.mp3", // Corrected path
       albumArt: defaultCover, // Placeholder image
     },
     {
       title: "Hidda Hiddai",
       artist: "1974 AD",
-      src: "./public/Hidda Hiddai.mp3", // Corrected path
+      src: "./assets/songs/Hidda Hiddai.mp3", // Corrected path
       albumArt: "https://res.cloudinary.com/db4vgbevh/image/upload/v1698665315/chordograph/albums/llmqjjtycpp1swadaizg.jpg", // Placeholder image
     },
   ]
